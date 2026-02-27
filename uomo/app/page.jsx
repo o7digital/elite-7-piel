deb3872060b11721956fd11af1b878de3c6c2953
o7digital <101292703+o7digital@users.noreply.github.com>
2026-02-27 16:20:15 -0600

    Add Uomo template and set Home 15 as default

---
+++ uomo/app/page.jsx
@@ -0,0 +1,13 @@
+import HomePage15 from "./(homes)/home-15/page";
+
+export const metadata = {
+  title: "Home 15 || Uomo eCommerce React Nextjs Template",
+  description: "Uomo eCommerce React Nextjs Template",
+};
+export default function Home() {
+  return (
+    <>
+      <HomePage15 />
+    </>
+  );
+}
