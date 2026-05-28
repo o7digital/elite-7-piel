2191f90c4e54ad0ee28f2514d58067b8d764ab74
o7digital <101292703+o7digital@users.noreply.github.com>
2026-05-28 15:58:27 -0600

    fix(hero): use direct Smart Slider iframe endpoint

--- uomo/components/homes/home-15/Hero.jsx
+++ uomo/components/homes/home-15/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
       style={{ lineHeight: 0, minHeight: "360px", background: "#f3f3f3" }}
     >
       <iframe
-        src="https://oliviers52.sg-host.com/?n2g=2"
+        src="https://oliviers52.sg-host.com/?n2prerender=1&n2app=smartslider&n2controller=slider&n2action=iframe&sliderid=2&hash=26e4bc6b941926287a7281c60d9238d3"
         title="Smart Slider 3 Hero"
         style={{ width: "100%", height: "min(70vh, 800px)", border: 0 }}
         loading="eager"
